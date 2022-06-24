0ca01d3f2c05e2fd5dfd73d55d48cb5245bea780
Aruming <0430kar@naver.com>
2022-06-24 21:35:04 +0900

    220624 백준 #2562

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,21 @@
 import java.util.Scanner;
 
 public class Main{
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        int n, i, min, max;
-        n = sc.nextInt();
-        int num[] = new int[n];
-
-        for(i=0;i<n;i++) {
+        int[] num = new int[9];
+        for(int i=0;i<9;i++){
             num[i] = sc.nextInt();
         }
-
-        min = num[0];
-        max = num[0];
-
-        for(i=0;i<n;i++) {
-            if(max<num[i]) {
+        int max = num[0];
+        int result = 0;
+        for(int i=0;i<9;i++){
+            if(num[i]>max) {
                 max = num[i];
-            }
-            if(min>num[i]) {
-                min = num[i];
+                result = i;
             }
         }
+        System.out.println(max+"\n"+(result+1));
 
-        System.out.println(min+" "+max);
     }
-
 }

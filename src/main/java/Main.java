76b201da8e286336d9f60304cca4e9077556c479
Aruming <0430kar@naver.com>
2022-06-24 21:34:11 +0900

    220624 백준 #10818

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,31 @@
-public class Main {
+import java.util.Scanner;
+
+public class Main{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n, i, min, max;
+        n = sc.nextInt();
+        int num[] = new int[n];
+
+        for(i=0;i<n;i++) {
+            num[i] = sc.nextInt();
+        }
+
+        min = num[0];
+        max = num[0];
+
+        for(i=0;i<n;i++) {
+            if(max<num[i]) {
+                max = num[i];
+            }
+            if(min>num[i]) {
+                min = num[i];
+            }
+        }
+
+        System.out.println(min+" "+max);
+    }
+
 }

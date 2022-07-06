1e775ff4317cdf3ff448902f816843ab4f73301b
Aruming <0430kar@naver.com>
2022-07-06 17:55:29 +0900

    #1546 평균

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] num = new int[10];
-        int[] arr = new int[42];
-        int result = 0;
+        //배열 크기 입력
+        int n = sc.nextInt();
+        double[] score = new double[n];
+        double max = 0;
 
-        for (int i = 0; i < 10; i++) {
-            num[i] = sc.nextInt();
-            arr[num[i] % 42]++;
+        //배열 값 입력, 배열 최댓값 구하기
+        for(int i=0;i<n;i++){
+            score[i] = sc.nextInt();
+            if(max<score[i])
+                max=score[i];
         }
-        for (int i = 0; i < 42; i++) {
-            if (arr[i] != 0)
-                result++;
+
+        //수정된 배열 값의 합
+        double sum=0;
+        for(int i=0;i<n;i++){
+            sum+=(score[i]/max)*100;
         }
-        System.out.println(result);
+        System.out.println(sum/n);
+
     }
 }

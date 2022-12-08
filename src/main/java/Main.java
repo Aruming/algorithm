e4bdd4d9ad69d9b5d2242d0478a2a9681a33a95c
Aruming <0430kar@naver.com>
2022-12-08 20:31:17 +0900

    #2480 주사위 세개

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("         ,r'\"7\n" +
-                "r`-_   ,'  ,/\n" +
-                " \\. \". L_r'\n" +
-                "   `~\\/\n" +
-                "      |\n" +
-                "      |");
+        int answer = 0;
+        Scanner sc = new Scanner(System.in);
+
+        String dice = sc.nextLine();
+        String[] result = dice.split(" ");
+        Arrays.sort(result);
+
+        if(result[0].equals(result[2])){
+            answer = 10000 + 1000 * Integer.parseInt(result[2]);
+        } else if (result[0].equals(result[1]) || result[1].equals(result[2])) {
+            answer = 1000 + 100 * Integer.parseInt(result[1]);
+        }else {
+            answer = 100 * Integer.parseInt(result[2]);
+        }
+
+
+        System.out.println(answer);
     }
 }

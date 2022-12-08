7ef24cd4da77eaf560bea1013649b2150049b3a3
Aruming <0430kar@naver.com>
2022-12-08 21:05:43 +0900

    #10807 개수 세기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,24 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String answer = "";
+        int answer = 0;
         Scanner sc = new Scanner(System.in);
 
-        int totalPrice = sc.nextInt();
         int n = sc.nextInt();
-
-        int calcPrice = 0;
+        int[] nums = new int[n];
 
         for(int i=0;i<n;i++){
-            int price = sc.nextInt();
-            int cnt = sc.nextInt();
-            calcPrice += price * cnt;
+            nums[i] = sc.nextInt();
         }
 
-        if(calcPrice == totalPrice){
-            answer = "Yes";
-        }else{
-            answer = "No";
+        int findNum = sc.nextInt();
+        for(int i=0;i<n;i++){
+            if(nums[i] == findNum) answer++;
         }
 
 

a262753abe6b408cc03891dee9f6e986f185eb59
Aruming <0430kar@naver.com>
2022-12-08 20:16:48 +0900

    #25083 ì새싹

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,10 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        String answer = "";
-        Scanner sc = new Scanner(System.in);
-        String pieces = sc.nextLine();
-
-        String[] piece = pieces.split(" ");
-        int[] num = {1, 1, 2, 2, 2, 8};
-
-        int own = 0;
-        for(int i=0;i<piece.length;i++){
-            own = Integer.parseInt(piece[i]);
-            answer += num[i]-own;
-            if(i == piece.length-1){
-                break;
-            }
-            answer += " ";
-        }
-
-        System.out.println(answer);
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
     }
 }

8dab24b21b8ff41bfe4e99e55f32c93dd393daf1
Aruming <0430kar@naver.com>
2022-12-09 23:51:26 +0900

    #2738 행렬 덧셈

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,17 +4,32 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] nums = new int[30];
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
-        for(int i=0;i<28;i++){
-            int n = sc.nextInt();
-            nums[n-1] = 1;
+        int[][] a = new int[n][m];
+        int[][] b = new int[n][m];
+
+        for (int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                a[i][j] = sc.nextInt();
+            }
         }
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i] == 0){
-                System.out.println(i+1);
+        int[][] result = new int[n][m];
+
+        for (int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                b[i][j] = sc.nextInt();
+                result[i][j] = a[i][j] + b[i][j];
             }
+        }
+
+        for (int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(result[i][j]+" ");
+            }
+            System.out.println("");
         }
     }
 }

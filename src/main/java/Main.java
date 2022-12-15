0644c1566beda895504411c02890b45c196bd849
Aruming <0430kar@naver.com>
2022-12-15 16:21:22 +0900

    #11650 좌표 정렬하기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
+
         int n = sc.nextInt();
 
-        String s = Integer.toString(n);
-        String[] nums = s.split("");
-        Arrays.sort(nums);
+        int[][] point = new int[n][2];
 
-        s = "";
-        for(int i = nums.length-1;i>=0;i--){
-            s += nums[i];
+        for(int i=0;i<n;i++) {
+            point[i][0] = sc.nextInt();
+            point[i][1] = sc.nextInt();
         }
 
-        int answer = Integer.parseInt(s);
-        System.out.println(answer);
+        Arrays.sort(point, (e1, e2) -> {
+            if(e1[0] == e2[0]) {
+                return e1[1] - e2[1];
+            } else {
+                return e1[0] - e2[0];
+            }
+        });
+
+        for(int i=0;i<n;i++) {
+            System.out.println(point[i][0] + " " + point[i][1]);
+        }
     }
 }

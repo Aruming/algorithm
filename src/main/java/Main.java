b475b8184838fe24202b08d5c9a2e6d739de1217
Aruming <0430kar@naver.com>
2023-01-02 21:10:12 +0900

    #2108 통계학

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,59 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        Scanner sc = new Scanner(System.in);
 
-        int n = Integer.parseInt(br.readLine());
+        int n = sc.nextInt();
+        List<Integer> list = new ArrayList<>();
 
-        int[] arr = new int[n];
-        int[] sortedArr = new int[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int sum = 0;
+        int mode = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
         for(int i=0;i<n;i++){
-            arr[i] = sortedArr[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(sortedArr);
+            int value = sc.nextInt();
+            sum += value;
+            list.add(value);
+            hashMap.put(value, hashMap.getOrDefault(value, 0)+1);
 
-        Map<Integer, Integer> rankingMap = new HashMap<>();
-        int rank = 0;
-        for(int i=0;i<n;i++){
-            if(!rankingMap.containsKey(sortedArr[i])){
-                rankingMap.put(sortedArr[i], rank);
-                rank++;
+            if(min > value){
+                min = value;
+            }
+            if(max < value){
+                max = value;
             }
         }
 
-        for(int i=0;i<n;i++){
-            sb.append(rankingMap.get(arr[i])).append(" ");
+        int avg = (int)Math.round((double)sum/n);
+        Collections.sort(list);
+        int median = list.get(n/2);
+
+        int numbers = 0;
+        for(int key : hashMap.values()) {
+            numbers = Math.max(numbers, key);
         }
 
-        System.out.println(sb);
+        list.clear();
+        for(int key : hashMap.keySet()) {
+            if(hashMap.get(key) == numbers) {
+                list.add(key);
+            }
+        }
+
+        Collections.sort(list);
+
+        if(list.size() >= 2) {
+            mode = list.get(1);
+        }
+        else {
+            mode = list.get(0);
+        }
+
+        System.out.println(avg);
+        System.out.println(median);
+        System.out.println(mode);
+        System.out.println(max-min);
     }
 }

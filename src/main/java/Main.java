e7310a718a39ed2b6fd46825748731be112b8100
Aruming <0430kar@naver.com>
2023-01-03 20:32:41 +0900

    #25501 재귀의 귀재

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,59 +1,24 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) throws Exception{
+public class Main{
+    static int recurs_num;
+    public static int recursion(String s, int l, int r){
+        recurs_num++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        List<Integer> list = new ArrayList<>();
 
-        int sum = 0;
-        int mode = 0;
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
         for(int i=0;i<n;i++){
-            int value = sc.nextInt();
-            sum += value;
-            list.add(value);
-            hashMap.put(value, hashMap.getOrDefault(value, 0)+1);
-
-            if(min > value){
-                min = value;
-            }
-            if(max < value){
-                max = value;
-            }
+            String str = sc.next();
+            recurs_num = 0;
+            System.out.println(isPalindrome(str)+" "+recurs_num);
         }
-
-        int avg = (int)Math.round((double)sum/n);
-        Collections.sort(list);
-        int median = list.get(n/2);
-
-        int numbers = 0;
-        for(int key : hashMap.values()) {
-            numbers = Math.max(numbers, key);
-        }
-
-        list.clear();
-        for(int key : hashMap.keySet()) {
-            if(hashMap.get(key) == numbers) {
-                list.add(key);
-            }
-        }
-
-        Collections.sort(list);
-
-        if(list.size() >= 2) {
-            mode = list.get(1);
-        }
-        else {
-            mode = list.get(0);
-        }
-
-        System.out.println(avg);
-        System.out.println(median);
-        System.out.println(mode);
-        System.out.println(max-min);
     }
 }

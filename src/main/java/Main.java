4e266acaa0d61138ea3756540ed8d65c6cf12c65
Aruming <0430kar@naver.com>
2023-01-04 16:18:34 +0900

    #24060 알고리즘 수업 - 병합정렬 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,67 @@
 import java.util.Scanner;
 
 public class Main{
-    static int recurs_num;
-    public static int recursion(String s, int l, int r){
-        recurs_num++;
-        if(l >= r) return 1;
-        else if(s.charAt(l) != s.charAt(r)) return 0;
-        else return recursion(s, l+1, r-1);
-    }
-    public static int isPalindrome(String s){
-        return recursion(s, 0, s.length()-1);
-    }
+    private static int[] tmp;
+    private static int cnt = 0;
+    private static int k;
+    private static int result = -1;
+
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        k = sc.nextInt();
 
+        int[] arr = new int[n];
+        tmp = new int[n];
         for(int i=0;i<n;i++){
-            String str = sc.next();
-            recurs_num = 0;
-            System.out.println(isPalindrome(str)+" "+recurs_num);
+            arr[i] = sc.nextInt();
+        }
+
+        sc.close();
+
+        merge_sort(arr, 0, arr.length-1);
+        System.out.println(result);
+    }
+
+    private static void merge_sort(int[] arr, int p, int r) {
+        if(p < r){
+            int q = (p + r) / 2;
+            merge_sort(arr, p, q);
+            merge_sort(arr, q+1, r);
+            merge(arr, p, q, r);
+        }
+    }
+
+    private static void merge(int[] arr, int p, int q, int r) {
+        int i = p;
+        int j = q + 1;
+        int t = 0;
+
+        while (i <= q && j <= r){
+            if(arr[i] <= arr[j]){
+                tmp[t++] = arr[i++];
+            }
+            else{
+                tmp[t++] = arr[j++];
+            }
+        }
+
+        while (i <= q){
+            tmp[t++] = arr[i++];
+        }
+
+        while (j <= r){
+            tmp[t++] = arr[j++];
+        }
+
+        i = p; t = 0;
+        while (i <= r){
+            cnt++;
+            if(cnt == k){
+                result = tmp[t];
+                break;
+            }
+            arr[i++] = tmp[t++];
         }
     }
 }

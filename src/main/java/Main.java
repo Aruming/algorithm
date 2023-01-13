3195590a91a5335055e1229ed48b12d6ee489b4b
Aruming <0430kar@naver.com>
2023-01-13 22:41:02 +0900

    #14425 문자열 집합

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,56 +1,26 @@
-import java.io.*;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        int[] cards = new int[n];
+        Scanner sc = new Scanner(System.in);
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        String[] words = new String[n];
+
         for(int i=0;i<n;i++){
-            cards[i] = Integer.parseInt(st.nextToken());
+            words[i] = sc.next();
         }
-        Arrays.sort(cards);
 
-        int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine());
-        for (int i=0;i<m;i++){
-            int num = Integer.parseInt(st.nextToken());
-            boolean result = binarySearch(cards, num);
-
-            if(result == true){
-                bw.write("1 ");
-            }else{
-                bw.write("0 ");
+        int cnt = 0;
+        for(int i=0;i<m;i++){
+            String check = sc.next();
+            for(int j=0;j<n;j++){
+                if(words[j].equals(check)) cnt++;
             }
         }
 
-        bw.close();
-        br.close();
-    }
-
-    public static boolean binarySearch(int[] cards, int num){
-        int start = 0;
-        int end = cards.length - 1;
-        int mid = (start + end) / 2;
-
-        while (start <= end){
-            if(cards[mid] == num){
-                return true;
-            }
-
-            if(cards[mid] > num){
-                end = mid - 1;
-            }else {
-                start = mid + 1;
-            }
-
-            mid = (start + end) / 2;
-        }
-        return false;
+        System.out.println(cnt);
     }
 }

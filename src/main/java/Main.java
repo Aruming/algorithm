f034bdb7888c5cdab0ade43f10838fed8bd12303
Aruming <0430kar@naver.com>
2023-02-21 23:30:35 +0900

    #11382 꼬마 정민

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,13 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        String[] words = new String[n];
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
 
-        for(int i=0;i<n;i++){
-            words[i] = sc.next();
-        }
-
-        int cnt = 0;
-        for(int i=0;i<m;i++){
-            String check = sc.next();
-            for(int j=0;j<n;j++){
-                if(words[j].equals(check)) cnt++;
-            }
-        }
-
-        System.out.println(cnt);
+        System.out.println(a+b+c);
     }
 }

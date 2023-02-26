f4d3b1a43491b18509a3dc858eba1d9fd44b142e
Aruming <0430kar@naver.com>
2023-02-26 13:12:35 +0900

    #10798 세로읽기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,29 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        while (sc.hasNextLine()){
-            String s = sc.nextLine();
-            System.out.println(s);
+        char[][] words = new char[5][15];
+
+        for (int i=0;i<5;i++){
+            String s = sc.next();
+            int len = 0;
+
+            for (int j=0;j<s.length();j++){
+                words[i][j] = s.charAt(j);
+            }
         }
+
+        String answer = "";
+        for(int i=0;i<15;i++){
+            for(int j=0;j<5;j++){
+                if(words[j][i] == '\0'){
+                    continue;
+                }else{
+                    answer += words[j][i];
+                }
+            }
+        }
+
+        System.out.println(answer);
 
         sc.close();
     }

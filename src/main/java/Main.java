96a295357528d10ae867aafea0f4b5e072327dbe
Aruming <0430kar@naver.com>
2023-02-26 12:56:10 +0900

    #11718 그대로 출력하기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,15 +4,11 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int t = sc.nextInt();
-
-        for(int i=0;i<t;i++){
-            String s = sc.next();
-            char start = s.charAt(0);
-            char end = s.charAt(s.length()-1);
-
-            System.out.print(start);
-            System.out.println(end);
+        while (sc.hasNextLine()){
+            String s = sc.nextLine();
+            System.out.println(s);
         }
+
+        sc.close();
     }
 }

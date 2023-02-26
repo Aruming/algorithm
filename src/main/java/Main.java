b48e52d961e3ef483471c027947fa21950550a31
Aruming <0430kar@naver.com>
2023-02-26 12:49:55 +0900

    #9086 문자열

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,15 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        String s = sc.next();
+        int t = sc.nextInt();
 
-        System.out.println(s.length());
+        for(int i=0;i<t;i++){
+            String s = sc.next();
+            char start = s.charAt(0);
+            char end = s.charAt(s.length()-1);
+
+            System.out.print(start);
+            System.out.println(end);
+        }
     }
 }
